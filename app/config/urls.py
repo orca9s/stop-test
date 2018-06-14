237ab774cbc4bc614593a574f810b29a1a137e18
sangmin <sang93423@gmail.com>
2018-06-15 02:01:58 +0900

    urlpatterns변경

--- app/config/urls.py
+++ app/config/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'', include('education.urls')),
-    # url(r'', include('education.urls')),
 ]
