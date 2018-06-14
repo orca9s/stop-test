2223df75d99ca39815191833fec9b591a489b65c
sangmin <sang93423@gmail.com>
2018-06-15 02:03:22 +0900

    school리스트 url추가, school디테일 리스트 추가, student리스트 url추가 student디테일 리스트 url추가

--- app/education/urls.py
+++ app/education/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from education.views import school_list, student_list
+from education.views import school_list, student_list, school_detail, student_detail
 
 urlpatterns = [
-    url(r'school/', school_list),
-    url(r'student/', student_list)
+    url(r'^school/$', school_list),
+    url(r'^school/(\d)/$', school_detail),
+    url(r'^student/$', student_list),
+    url(r'^student/(\d)/$', student_detail)
 ]
